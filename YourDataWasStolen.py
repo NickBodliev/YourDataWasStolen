edcf21cb8c39c1af73b062c8120d569c8f723b71
unknown <bodliev.mykola@gmail.com>
2021-04-24 21:43:43 +0200

    Correcting bad functionalòity of whule not loop

--- YourDataWasStolen.py
+++ YourDataWasStolen.py
@@ -2,12 +2,12 @@ import requests
 import cloudscraper
 import re
 
-regex = '^(\w|\.|\_|\-)+[@](\w|\_|\-|\.)+[.]\w{2,3}$'
+notMailChars = '^(\w|\.|\_|\-)+[@](\w|\_|\-|\.)+[.]\w{2,3}$'
 url = 'https://haveibeenpwned.com/unifiedsearch/'
 
-def check(email):
-    if re.search(regex, email):
-        return True
+def check(str):                     #check if the passed string is an email (not contains forbiden signs)
+    if re.search(notMailChars, str):    
+        return True                 
     else:
         return False
         
@@ -16,14 +16,16 @@ scraper = cloudscraper.create_scraper()  #module to bypass Cloudflare's anti-bot
 print("533 million Facebook users' phone numbers and email addresses have been leaked online! Checke if yours are sefe!")
 
 data = input("Input the mail/phone number you want to check:")
-while not "+" in data or "@" in data:
-    if data.lower().islower():                  #mail
+
+#check of the format of the written data
+while not ("+" in data or "@" in data): #control if written data hasn't any @ sign (for email addresses) or + sign (for phone numbers)                    
+    if data.lower().islower():                      #if this condition is true, the input data contains letters, so it's an email address
         data = input("Invalid email! Try again:")
-    else:                                   #phone number 
+    else:                                           #else it's a phone number 
         data = input("Invalid phone number! Try again:")
 
-formatedMail = data.replace('@','%40')
-url = url + formatedMail
+formatedData = data.replace('@','%40')
+url = url + formatedData
 r = scraper.get(url)
 if r.status_code == 404:
     print("Your data is safe")
